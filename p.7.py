29322e207a9da604cdfa7e663761c545e056761d
Dariel Zaid Laguna Barcenas <com.dlagunab24@alumnos.cecyteg.edu.mx>
2025-03-11 16:53:48 -0600

    Add files via upload

---
+++ p.7.py
@@ -0,0 +1,42 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============== RESTART: C:/Users/Alumno/Downloads/practica 6....py =============
+
+hola 
+dariel zaid 
+tienes 
+16 
+años
+>>> 
+=============== RESTART: C:/Users/Alumno/Downloads/practica 7.py ===============
+python-es-genial
+>>> 
+============== RESTART: C:/Users/Alumno/Downloads/practica 6....py =============
+
+hola 
+dariel zaid 
+tienes 
+16 
+años
+hola-dariel zaid-tienes-16-años
+>>> 
+============== RESTART: C:/Users/Alumno/Downloads/practica 6....py =============
+
+hola 
+dariel zaid 
+tienes 
+16 
+años
+hola
+dariel zaid
+tienes
+16
+años
+>>> 
+=============== RESTART: C:/Users/Alumno/Downloads/practica 7.py ===============
+python-es-genial
+nombre	edad	ciudad
+dariel	16	mexico
+mario	30	paris
+luisa	18	honduras
